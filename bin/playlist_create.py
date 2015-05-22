68c8c0438e4d77b6bc62bea927d863c463f9585b
Franco Reyes <freyes1988@gmail.com>
2015-05-21 23:42:01 -0500

    Have the playlist actually parse the file

--- bin/playlist_create.py
+++ bin/playlist_create.py
@@ -1,16 +1,33 @@
 import os
 import json
+import sys
 
 from gmusicapi import Mobileclient
 
+with open('../config/login.json') as data_file:
+    credentials = json.load(data_file)
+
+# Retrieve input file
+print str(sys.argv)
+print str(credentials)
+
+song_list = sys.argv[1]
+
 api = Mobileclient()
+api.login(credentials['login'], credentials['password'])
 
-api.login('user@gmail.com', 'my-password')
-# => True
+with open(song_list) as input_file:
+    for line in input_file:
+        if line[0] == '#' or line == '\n':
+            continue
+        else:
+            line = line.rstrip('\n')
+            song, artist, option = line.split('|')
+            song = song.strip()
+            artist = artist.strip()
+            option = option.strip()
 
-library = api.get_all_songs()
-sweet_track_ids = [track['id'] for track in library
-                   if track['artist'] == 'The Cat Empire']
+            # Begin searching
 
-playlist_id = api.create_playlist('Rad muzak')
-api.add_songs_to_playlist(playlist_id, sweet_track_ids)
+
+
